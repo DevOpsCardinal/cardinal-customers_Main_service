9d0268540bbbd96616d07fae72cead4f18189f9b
Alejandro Personal <Alejoax70@gmail.com>
2026-02-06 09:45:47 -0500

    cambio de ip al servidor

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:8081", "http://localhost:8081"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
